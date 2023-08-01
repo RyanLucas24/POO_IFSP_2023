b7c41e01f16362daee437e382f86bae950ba2095
Ryan Lucas <ryanlucaspatron@gmail.com>
2023-08-01 17:47:50 -0300

    POO_2023

---
+++ src/Codes/Exercise_01.java
@@ -0,0 +1,7 @@
+package Codes;
+
+public class Exercise_01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Codes/Hello_world.java
@@ -0,0 +1,7 @@
+package Codes;
+
+public class Hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
