47982aaca3283805f159bea24bf63f92c3d495c8
Ryan Lucas <ryanlucaspatron@gmail.com>
2023-08-06 16:19:46 -0300

    Update04.java

--- src/Codes/Semana_01/list01_exercise/Ex04.java
+++ src/Codes/Semana_01/list01_exercise/Ex04.java
@@ -1,5 +1,6 @@
 package Codes.Semana_01.list01_exercise;
 
+
 import java.util.Scanner;
 
 /*
@@ -7,7 +8,7 @@ import java.util.Scanner;
     A loja oferece um desconto: para cada M embalagens que ele dá para a loja, ele ganha um chocolate grátis.
     Quantos chocolates Osmar consegue comer? Por exemplo:
 
-    Para N=10, C=2, M=5, ele pode comprar 5 chocolates por $10 e trocar as 5 embalagens por mais 1 chocolate,
+    Para N=10, C=2, M=5, ele pode comprar5  chocolates por $10 e trocar as 5 embalagens por mais 1 chocolate,
     fazendo com que o número total de chocolates que ele pode comer seja 6.
     Faça um programa que leia inteiros N, C e M e imprima a quantidade de chocolates que Osmar pode comer.
     C e M são inteiros positivos.
@@ -36,10 +37,25 @@ public class Ex04 {
 
     int compute(int n, int c, int m) {
 
-        if(c < 0 || m < 0) return 0;
-        int qtd = n/c;
-        int bonus = m/c;
-        int total = qtd + bonus;
+        int qtd = n / c;
+
+        int bonus = qtd / m;
+
+        int sobra = qtd % m;
+
+        int total = qtd + bonus ;
+
+        if((bonus + sobra)/m >= 1){
+
+            if(sobra >= 1) total+=sobra;
+
+            while(bonus/m >= 1) {
+                total += bonus / m;
+                sobra = bonus % m;
+                bonus = bonus / m + sobra;
+            }
+
+        }
 
         return total;
     }
